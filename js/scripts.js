8caa6f2e99d018a033cb34eb2b4da5cb60aea7e7
Wairimu Karuga <wairimukaruga4@gmail.com>
2021-03-01 15:42:55 +0300

    add a toggle effect between icons and text

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-jQuery(document).ready(function() {
+$(document).ready(function() {
 
+  $("#icon1").click(function() {
+    $(".hidden1").toggle();
+  });
+  $("#icon2").click(function() {
+    $(".hidden2").toggle();
+  });
+  $("#icon3").click(function() {
+    $(".hidden3").toggle();
+  });
   
 
-})
+});
